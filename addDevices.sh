11b8c00aff9afa0f1c9f331cd8a344fb29d266b6
mounika120 <64479944+mounika120@users.noreply.github.com>
2020-12-14 16:51:28 +0100

    Update addDevices.sh

--- addDevices.sh
+++ addDevices.sh
@@ -1,4 +1,15 @@
+
 #!/bin/bash
 
-#curl "localhost:8000/addDevice.php?ip=0.0.0.0&port=1023&community=public&version=2"
-curl "localhost:8000/addDevice.php?ip=127.0.0.1&port=1024&community=public&version=2"
+php -S "localhost:3000" &
+curl "localhost:3000/config.php"
+
+#curl "localhost:3000/addDevice.php?ip=192.168.0.106&port=1024&community=public&version=2"
+curl "localhost:3000/addDevice.php?ip=192.168.0.106&port=1024&community=device_192_168_184_21&version=2"
+curl "localhost:3000/addDevice.php?ip=192.168.0.106&port=1024&community=device_192_168_184_39&version=2"
+curl "localhost:3000/addDevice.php?ip=192.168.0.106&port=10246&community=device_192_168_184_98&version=2"
+curl "localhost:3000/addDevice.php?ip=192.168.0.106&port=1024&community=device_192_168_184_23&version=2"
+curl "localhost:3000/addDevice.php?ip=192.168.0.106&port=1024&community=device_192_168_184_40&version=2"
+curl "localhost:3000/addDevice.php?ip=192.168.0.106&port=1024&community=device_192_168_184_38&version=2"
+curl "localhost:3000/addDevice.php?ip=192.168.0.106&port=1024&community=device_192_168_184_23&version=2"
+curl "localhost:3000/addDevice.php?ip=192.168.0.106&port=1024&community=device_192_168_184_21&version=2"
