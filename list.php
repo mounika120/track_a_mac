053d242d5fae397f27223d7ce6bf4ad789274393
mounika120 <64479944+mounika120@users.noreply.github.com>
2020-12-14 16:56:30 +0100

    update list

--- list.php
+++ list.php
@@ -5,7 +5,7 @@ include('config.php');
 $result = $db->query('SELECT * FROM List');
 
 while($row = $result->fetchArray(SQLITE3_ASSOC) ) {
-	echo  $row['DeviceIP']. "|" .$row['VLANs']. "|" .$row['PORT']. "|" .$row['MACS']. "\n";
+	echo  $row['IP']. "|" .$row['VLANs']. "|" .$row['PORT']. "|" .$row['MACS']. "\n";
 }
 $db->close();
 
